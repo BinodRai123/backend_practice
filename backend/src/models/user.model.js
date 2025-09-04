ed351ce0f9d83e0f5f077bd4dccac4aa4e545200
binod rai <br163406@gmail.com>
2025-09-04 22:00:59 +0545

    mongoose validation applied

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -2,8 +2,15 @@ const mongoose = require("mongoose");
 
 
 const userSchema = new mongoose.Schema({
-    name:String,
-    password:Number,
+    name:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
 })
 
 const userModel = mongoose.model("users",userSchema);
